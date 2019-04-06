102f3d32770b2b1daf950a76e08662ada17795a9
Luc Gommans <git-client@lgms.nl>
2019-04-06 13:48:35 +0200

    Improve page titles

--- index.php
+++ index.php
@@ -1,24 +1,45 @@
+<?php
+	if (isset($_GET['code'])) {
+		// Someone is giving feedback
+		$title = 'Feedback :: ';
+		$state = 1;
+	}
+	else if (isset($_GET['secret'])) {
+		// The presentation owner is viewing the results
+		$title = 'Results :: ';
+		$state = 2;
+	}
+	else if (isset($_POST['title'])) {
+		// A new presentation is being created
+		$title = 'Create :: ';
+		$state = 3;
+	}
+	else {
+		$title = '';
+		$state = 0;
+	}
+?>
 <!DOCTYPE html>
 <html>
 	<head>
 		<meta charset='utf-8'/>
-		<title>Presentation Feedback Tool</title>
+		<title><?php echo $title; ?>Presentation Feedback Tool</title>
 	</head>
 	<body style="margin: 0 auto 0 auto; width: 720px; margin-top: 100px;">
 		<?php 
 			$included = true;
-			if (isset($_POST['title']) || isset($_GET['code']) || isset($_GET['secret'])) {
+			if ($state > 0) {
 				require('config.php');
 				require('dbsetup.php');
 			}
 
-			if (isset($_POST['title'])) {
+			if ($state == 3) {
 				require('register.php');
 			}
-			else if (isset($_GET['code'])) {
+			else if ($state == 1) {
 				require('feedback.php');
 			}
-			else if (isset($_GET['secret'])) {
+			else if ($state == 2) {
 				require('results.php');
 			}
 			else {
@@ -31,3 +52,4 @@
 		</footer>
 	</body>
 </html>
+
